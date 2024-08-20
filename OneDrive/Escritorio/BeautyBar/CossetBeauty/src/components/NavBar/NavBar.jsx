79e049229093b766fbd6407bfc27c63e9af726a7
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-08-19 21:05:57 -0600

    components

---
+++ OneDrive/Escritorio/BeautyBar/CossetBeauty/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+    return (
+      <nav>
+        <h1>Cosset Beauty Bar</h1>
+        <div>
+            <button>Unas</button>
+            <button>Corporales</button>
+            <button>Depilacion</button>
+        </div>
+      </nav>
+    )
+  }
+  export default NavBar
